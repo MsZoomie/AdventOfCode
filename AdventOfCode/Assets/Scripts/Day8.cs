e34b9ad5564bc94a07a9aa34612b34d66c6afe5e
Sara Uvalic <sara.uvalic@hotmail.com>
2020-09-06 15:01:02 +0200

    started on day 8

---
+++ AdventOfCode/Assets/Scripts/Day8.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Day8 : MonoBehaviour
+{
+    int width = 3;
+    int height = 2;
+
+    public void DecodeA(TextAsset puzzleInput)
+    {
+        char[] characters = puzzleInput.text.ToCharArray();
+        List<char[]> imageLayers = new List<char[]>();
+
+        for (int i = 0; i < characters.Length; i += width*height)
+        {
+            char[] layer = new char[width * height];
+
+            if(characters.Length < i + (width * height))
+            {
+
+            }
+        }
+
+        
+        
+    }
+
+}
