a20372d45c1e7ab08410fc310593406556911228
Sara Uvalic <sara.uvalic@hotmail.com>
2020-09-07 09:57:37 +0200

    puzzle 8a complete

--- AdventOfCode/Assets/Scripts/Day8.cs
+++ AdventOfCode/Assets/Scripts/Day8.cs
@@ -5,26 +5,91 @@ using System;
 
 public class Day8 : MonoBehaviour
 {
-    int width = 3;
-    int height = 2;
+    public  int WIDTH = 25;
+    public  int HEIGHT = 6;
 
     public void DecodeA(TextAsset puzzleInput)
     {
-        char[] characters = puzzleInput.text.ToCharArray();
-        List<char[]> imageLayers = new List<char[]>();
-
-        for (int i = 0; i < characters.Length; i += width*height)
+        List<Layer> layers = BuildLayers(puzzleInput);
+        int fewestZeros = int.MaxValue;
+        foreach (var layer in layers)
         {
-            char[] layer = new char[width * height];
-
-            if(characters.Length < i + (width * height))
-            {
-
-            }
+            fewestZeros = Mathf.Min(fewestZeros, layer.zeros);
         }
 
-        
-        
+        Layer wantedLayer = layers.Find(x => x.zeros == fewestZeros);
+
+        string y = "";
+        for (int i = 0; i < wantedLayer.digits.Length; i++)
+        {
+            if (i % WIDTH == 0)
+                y += "\n";
+
+            y += wantedLayer.digits[i].ToString();
+        }
+        Debug.Log(y);
+
+        int answer = wantedLayer.ones * wantedLayer.twos;
+        Debug.Log(answer.ToString());
     }
 
+
+    public List<Layer> BuildLayers(TextAsset puzzleInput)
+    {
+        Layer currentLayer = new Layer();
+
+        int layerSize = WIDTH * HEIGHT;
+        char[] characters = puzzleInput.text.ToCharArray();
+
+        List<Layer> layers = new List<Layer>();
+        
+        currentLayer.digits = new int[layerSize];
+
+        for (int i = 0; i < characters.Length; i++)
+        {
+            int layerIndex = i % layerSize;
+            
+            if(layerIndex == 0)
+            {
+                if (i != 0)
+                {
+                    layers.Add(currentLayer);
+                   
+                }
+
+                currentLayer = new Layer();
+                currentLayer.digits = new int[layerSize];
+            }
+
+            currentLayer.digits[layerIndex] = (int)(characters[i] - '0');
+            switch (currentLayer.digits[layerIndex])
+            {
+                case 0:
+                    currentLayer.zeros++;
+                    break;
+                case 1:
+                    currentLayer.ones++;
+                    break;
+                case 2:
+                    currentLayer.twos++;
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        layers.Add(currentLayer);
+        return layers;
+    }
+
+    
+}
+
+public class Layer
+{
+    public int[] digits;
+    public int zeros;
+    public int ones;
+    public int twos;
 }
